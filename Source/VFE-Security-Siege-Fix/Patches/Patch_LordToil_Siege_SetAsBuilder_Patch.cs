ac4d5a549343268922f2eb26e1bb000e91bb6f0a
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-11-08 19:22:30 -0600

    The Patch

---
+++ Source/VFE-Security-Siege-Fix/Patches/Patch_LordToil_Siege_SetAsBuilder_Patch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using VFESecurity;
+
+namespace VFESecuritySiegeFix.Patches;
+
+// ReSharper disable InconsistentNaming
+
+[HarmonyPatch(typeof(LordToil_Siege_SetAsBuilder_Patch))]
+public class Patch_LordToil_Siege_SetAsBuilder_Patch
+{
+    // Have to use __args injection because the argument we want from the original is __instance
+    [HarmonyPatch(nameof(LordToil_Siege_SetAsBuilder_Patch.SetMinLevels))]
+    [HarmonyPrefix]
+    public static bool SetMinLevels_Prefix(object[] __args)
+    {
+        var __instance = (LordToil_Siege)__args[1];
+
+        // Bug in the original is, quite literally, just not checking to see if there are actually any blueprints
+        // before trying to find the max construction requirement of them.
+        if (((LordToilData_Siege)__instance.data).blueprints.EnumerableNullOrEmpty()) return false;
+
+        return true;
+    }
+}
---
+++ Source/VFE-Security-Siege-Fix/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace VFESecuritySiegeFix;
+
+[StaticConstructorOnStartup]
+public static class Startup
+{
+    static Startup()
+    {
+        Harmony.DEBUG = false;
+        var harmony = new Harmony("VFESecuritySiegeFix");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
